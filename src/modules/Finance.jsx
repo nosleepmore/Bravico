69467242e6043b00e1565388c62b1ade3a90728a
nosleepmore <you@example.com>
2025-05-27 10:22:40 +0300

    Добавлены модули

---
+++ src/modules/Finance.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Finance() {
+  return <div>Finance (модуль)</div>;
+}
---
+++ src/modules/Sales.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Sales() {
+  return <div>Sales (модуль)</div>;
+}
